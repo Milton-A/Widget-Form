93bb1303e4609e0d7479b8388e5952753e87fdd3
Milton-A <miltondantas33@gmail.com>
2022-07-24 12:10:58 +0100

    first commit

---
+++ src/components/WidgetForm/index.tsx
@@ -0,0 +1,66 @@
+import bugImageUrl from '../../assets/bug.svg';
+import ideaImageUrl from "../../assets/idea.svg";
+import thoughImageUrl from "../../assets/thought.svg";
+import { useState } from "react";
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+import { FeedbackSucessStep } from './Steps/FeedbackSucessStep';
+
+export const feedbackTypes = {
+  BUG: {
+    title: 'Problema',
+    image: {
+      source: bugImageUrl,
+      alt: 'Imagem de um Inseto'
+    }
+  },
+  IDEA: {
+    title: 'Ideia',
+    image: {
+      source: ideaImageUrl,
+      alt: 'Imagem de uma Lâmpada'
+    }
+  },
+  OTHERS: {
+    title: 'Outro',
+    image: {
+      source: thoughImageUrl,
+      alt: 'Imagem de um balão de pensamento'
+    }
+  },
+};
+
+export type FeedbackType = keyof typeof feedbackTypes;
+
+export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+  const [feedbackSent, setFeedbackSent] = useState(false);
+
+  function handleRestartFeedback() {
+    setFeedbackType(null);
+  }
+
+  return (
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+      {feedbackSent ? (
+        <FeedbackSucessStep />
+      ) : (
+        <>
+          {!feedbackType ? (
+            <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+          ) : (
+            <FeedbackContentStep
+              feedbackType={feedbackType}
+              onFeedbackRestartRequested={handleRestartFeedback}
+              onFeedbackSent={() => setFeedbackSent(true)}
+            />
+          )}
+
+        </>
+      )}
+      <footer>
+        Feito Com Amor por <a className="underline underline-offset-1" >Milton-A</a>
+      </footer>
+    </div>
+  );
+}
